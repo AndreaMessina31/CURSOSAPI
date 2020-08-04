288c82b02bc1c7c518d79f0127c82e5f2dd1718c
AndreaMessina31 <maktub_dharma@hotmail.com>
2020-08-03 21:09:33 -0300

    Creamos Categoria Repository

--- src/main/java/ar/com/ada/api/cursos/repos/CategoriaRepository.java
+++ src/main/java/ar/com/ada/api/cursos/repos/CategoriaRepository.java
@@ -1,9 +1,11 @@
 package ar.com.ada.api.cursos.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import ar.com.ada.api.cursos.entities.Categoria;
 
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+@Repository
+public interface CategoriaRepository extends JpaRepository <Categoria, Integer> {
     
 }
