42691e07050b8ce9fa8437138fdb7b61f5266f6f
AndreaMessina31 <maktub_dharma@hotmail.com>
2020-10-05 22:18:59 -0300

    La API PagADA  nos permitira integrar nuestro proceso de inscripcion con el proceso de pago. Cuando implemente mos una integracion de una API desde el backend,  hay que tener en claro que el "front" es nuestro backend, por lo cual hay que usar una libreria como la de UniRest para generar la llamada. Generalmente las Frontenders son las que integran APIs, usando JavaScript, ahora nosotras tendremos que integrar las APIs usando el lenguaje en el que hayan¿mos codeado el sistema. Recordar que en backedn una no necesariamente va a integrar muchas APIS, siempre va a depender del tipo de negocio que estemos buscando y si vale la pena que la hagamos directamente de 0. En el commit anterior se subieron unas notas.txt que tienen que ver sobre como integrariamos, que es la parte mas dificil de una integracion API porque es engancharla a un sistema ya existente. Se debe conocer como funciona la API externa y cuales metodos usar. Tomar como regla que siempre que hagan una integracion, va a ver un package y dentro de ese package un Service que controla todo. Dentro de ese package armaremos los models que sean necesarios, para poder consumir la API, las nomenclaturas de cada model ahora quedan abierta a cada una, asi que no necesariamente se tienen que regir por la misma que usaban, siempre que lo dejen dentro del package especifico. Como inicial, se subio el PagADAService, con lo que seria el metodo que genera un Deudor en base a un estudiante, obviamente falta trabajo por hacer  y terminar de armar los otros metodos(Alta Servicio y Pago) para luego engancharlo en nuestra API. En nuestro caso, vimos como la libreria UniREST Nos permite obtener JsonNodes y JsonObjects qeu seran los objetos que manejaremos para leer los resultados. Tambien podriamos desserializar el mismo a un objeto directo de nuestro sistema(tiene que coincidir los campos o usar un converter)

--- src/main/java/ar/com/ada/api/cursos/pagada/ResultadoCreacionDeudor.java
+++ src/main/java/ar/com/ada/api/cursos/pagada/ResultadoCreacionDeudor.java
@@ -1,5 +1,9 @@
 package ar.com.ada.api.cursos.pagada;
 
+
+/***
+ * Esto devuelve la api de Pagada al crear un deudor.
+ */
 public class ResultadoCreacionDeudor {
     public Boolean isOk;
     public Integer id;
